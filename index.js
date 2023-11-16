07cc2a292dde52e0071fb1788e4c048128686f33
avideb24 <debavi24@gmail.com>
2023-11-16 14:02:31 +0600

    updted users api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -34,6 +35,13 @@ async function run() {
     const reviewCollection = client.db("tastycDB").collection("reviews");
     const cartCollection = client.db("tastycDB").collection("carts");
 
+    
+    // user get
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
     // user post
     app.post('/users', async(req, res) => {
       const user = req.body;
@@ -47,11 +55,27 @@ async function run() {
       res.send(result);
     })
 
-    // user get
-    app.get('/users', async(req, res) => {
-      const result = await userCollection.find().toArray();
+    // user patch (make admin)
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin"
+        }
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     })
+
+    // user delete
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // menu get
     app.get('/menu', async(req, res) => {
